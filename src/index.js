36409105f781bc6b78c07b707cc3e62c7af32f5b
Sachin Venugopalan <sachinvenugopalan99@gmail.com>
2023-04-22 12:16:43 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/carts' element={<Cart />} />
